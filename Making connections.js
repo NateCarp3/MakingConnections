d3f1867ce585e494bcd16103b5574fd1aeff9347
Nathan  Carpenter <96751531+NateCarp3@users.noreply.github.com>
2022-02-10 17:11:40 -0500

    Add files via upload

---
+++ Making connections.js
@@ -0,0 +1,17 @@
+var JD = document.querySelector("#JD")
+var chex = document.querySelector(".card-list-item")
+var chex2 = document.querySelector(".card-list-item2")
+
+
+function nameChange(element){
+    JD.innerText = "Nathan Carpenter";
+}
+console.log("page loaded...");
+
+function hide(element){
+    chex.remove();
+}
+function hide2(element){
+    chex2.remove();
+
+}
